86439351cf37a361b08102087a4493407affca17
cydyldrm <elifceyda.yildirim@stu.pirireis.edu.tr>
2024-05-23 15:19:29 +0300

    feat: add image setting model

--- mycvvv/models.py
+++ mycvvv/models.py
@@ -34,9 +34,51 @@ class GeneralSetting(models.Model):
         verbose_name="created date",
 
     )
-def __str__(self):
-    return f"General Setting: {self.name}"
-class Meta:
-    verbose_name = 'General Setting'
-    verbose_name_plural = 'General Settings'
-    ordering = ('name', )
+    def __str__(self):
+        return f"General Setting: {self.name}"
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('name', )
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name="Name Test",
+        help_text="This is variable of the setting."
+    )
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name="Description",
+    )
+    file = models.FileField(
+        default="",
+        verbose_name="File",
+        blank=True,
+        help_text="",
+        upload_to='images/',
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name="updated date",
+
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name="created date",
+
+    )
+
+    def __str__(self):
+        return f"Image Setting: {self.name}"
+
+    class Meta:
+        verbose_name = 'Image Setting'
+        verbose_name_plural = 'Image Settings'
+        ordering = ('name',)
