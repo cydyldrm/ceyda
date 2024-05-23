25cd341c991a136d38b1dca9865a1bef372a0a1d
cydyldrm <elifceyda.yildirim@stu.pirireis.edu.tr>
2024-05-23 22:57:41 +0300

    refactor: apply image setting

--- mycvvv/views.py
+++ mycvvv/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from mycvvv.models import GeneralSetting, ImageSetting
+
+
 def index(request):
-    return render(request, 'index.html')
+    site_title = GeneralSetting.objects.get(name='site_title').parameter
+    site_keywords = GeneralSetting.objects.get(name='site_keywords').parameter
+    site_description = GeneralSetting.objects.get(name='site_description').parameter
+    home_banner_name = GeneralSetting.objects.get(name='home_banner_name').parameter
+    home_banner_title = GeneralSetting.objects.get(name='home_banner_title').parameter
+    home_banner_description = GeneralSetting.objects.get(name='home_banner_description').parameter
+    about_myself_footer = GeneralSetting.objects.get(name='about_myself_footer').parameter
+    about_myself_welcome = GeneralSetting.objects.get(name='about_myself_welcome').parameter
 
-
+    # Images
+    header_logo = ImageSetting.objects.get(name='header_logo').file
+    home_banner_image = ImageSetting.objects.get(name='home_banner_image').file
+    site_favicon = ImageSetting.objects.get(name='site_favicon').file
+    context = {
+        'site_title': site_title,
+        'site_keywords': site_keywords,
+        'site_description': site_description,
+        'home_banner_name': home_banner_name,
+        'home_banner_title': home_banner_title,
+        'home_banner_description': home_banner_description,
+        'about_myself_footer': about_myself_footer,
+        'about_myself_welcome': about_myself_welcome,
+        'header_logo': header_logo,
+        'home_banner_image': home_banner_image,
+        'site_favicon': site_favicon
+    }
+    return render(request, 'index.html', context=context)
 
 # Create your views here.
